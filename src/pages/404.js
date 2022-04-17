729e8b3b7eaf3e022a0abdb234728cc2b4dbcf1a
mdsagar333 <md_sagar@yahoo.com>
2022-04-17 23:13:44 +0600

    edited readMe file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,16 @@
+import { Link } from "gatsby"
 import React from "react"
 
 const NotFound = () => {
-  return <div>404 Not found</div>
+  return (
+    <div className="container mx-auto max-w-[1200px] p-5">
+      <h1 className="text-4xl mb-5 text-center">Page Not found</h1>
+      <p className="mb-5">Ooopss..</p>
+      <Link className="" to="/">
+        Go back
+      </Link>
+    </div>
+  )
 }
 
 export default NotFound
